766eeafc6484b9fdaba8107e5da076aadc7925a4
Neil Brags Guzman <neilbragsg@gmail.com>
2026-06-22 03:47:30 +0800

    added: DisplayPost function to diplay post

--- src/main.js
+++ src/main.js
@@ -1,3 +1,25 @@
 import './style.css';
 
+const button = document.getElementById('submitBtn')
+const input = document.getElementById('userInput')
+const feed = document.getElementById('displayFeed')
 
+function DisplayPost(postText) {
+    const newPost = document.createElement('p');
+    newPost.textContent = postText;
+    if (feed) {
+       feed.prepend(newPost); 
+    }
+    input.value = "";
+}
+
+function EnterAndSubmitPost() {
+    button.addEventListener('click', () => {
+        const inputVal = input.value;
+        if (inputVal.trim() == "") return;
+
+        DisplayPost(inputVal);
+    });
+};
+
+EnterAndSubmitPost();
